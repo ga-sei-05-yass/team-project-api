a5681024e307b682df63159e4ce72da60446e684
ssuchir <ssuchir@gmail.com>
2019-11-05 14:20:33 -0500

    Fixed routes and added credentialsTest

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
-const imageRoutes = require('./app/routes/user_routes')
+const imageRoutes = require('./app/routes/image_routes')
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
